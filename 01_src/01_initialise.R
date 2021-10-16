46eb347e3c863c3dd54a6da7fdfc1f9dce536b7e
Jason Pott <j.pott@qmul.ac.uk>
2021-10-16 21:52:54 +0100

    intitial setup

---
+++ 01_src/01_initialise.R
@@ -0,0 +1,8 @@
+#KNITR setup
+knitr::opts_chunk$set(echo = TRUE, warning=TRUE, message = TRUE)
+options(stringsAsFactors = FALSE)
+
+#Package load
+pacman::p_load(tidyverse, tidylog, kableExtra, lubridate, readxl, janitor)
+
+#Function load 
